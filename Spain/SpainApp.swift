72e71159194db7636193d996df815bf50cc9ec6d
max <1733169585qaq@gmail.com>
2026-01-06 13:48:04 +0800

    Initial Commit

---
+++ Spain/SpainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpainApp.swift
+//  Spain
+//
+//  Created by Max on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SpainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpainTests/SpainTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpainTests.swift
+//  SpainTests
+//
+//  Created by Max on 1/6/26.
+//
+
+import Testing
+@testable import Spain
+
+struct SpainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
